694c0e4e448cc17c139244f14e5e5977e6460076
Maryam Ansari <145547172+MarioSary@users.noreply.github.com>
2024-04-20 01:01:56 -0800

    Drag&Drop updated && Snap added

--- Assets/2- Scripts/DragAndDrop.cs
+++ Assets/2- Scripts/DragAndDrop.cs
@@ -5,111 +5,167 @@ using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
+    Vector3 offset;
+    public string destinationTag;
     
+    [SerializeField] private Vector3 productInFirstPos;
+    private bool dragging = false;
+ 
+    public void Update()
+    {
+        if (Input.touchCount != 1)
+        { 
+            dragging = false;
+            return;
+        }
+
+        if (Input.touches[0].phase == TouchPhase.Began)
+        {
+            OnMouseDown();
+            //CheckHitProduct();
+        }
+
+        if (dragging && Input.touches[0].phase == TouchPhase.Moved)
+        {
+            OnMouseDrag();
+            //DragProduct();
+        }
+
+        if (dragging && (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled))
+        {
+            OnMouseUp();
+            //DropProduct();
+        }
+            
+    }
+    void OnMouseDown()
+    {
+        offset = transform.position - TouchWorldPosition();
+        //transform.GetComponent<Collider>().enabled = false;
+    }
+ 
+    void OnMouseDrag()
+    {
+        transform.position = TouchWorldPosition() + offset;
+    }
+ 
+    void OnMouseUp()
+    {
+        var rayOrigin = Camera.main.transform.position;
+        var rayDirection = TouchWorldPosition() - Camera.main.transform.position;
+        RaycastHit hitInfo;
+        if(Physics.Raycast(rayOrigin, rayDirection, out hitInfo))
+        {
+            if(hitInfo.transform.tag == destinationTag)
+            {
+                transform.position = hitInfo.transform.position;
+                
+                //to lock on the correct pos
+                transform.GetComponent<Collider>().enabled = false;
+            }
+            else
+            {
+                transform.position = productInFirstPos;
+                dragging = false;
+            }
+        }
+        //transform.GetComponent<Collider>().enabled = true;
+    }
+ 
+    Vector3 TouchWorldPosition()
+    {
+        var touchScreenPos = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y,
+            Camera.main.WorldToScreenPoint(transform.position).z);
+        touchScreenPos.z = Camera.main.WorldToScreenPoint(transform.position).z;
+        return Camera.main.ScreenToWorldPoint(touchScreenPos);
+    }
+
+
+    /*//Drag
     private float dist;
     private bool dragging = false;
     private Vector3 offset;
     private Transform toDrag;
+    Vector3 v3;
     
-    //BACK TO POS IN WRONG SITUATIONS
-    private bool isLocked;
-    [SerializeField] private GameObject[] product;
-    [SerializeField] private GameObject[] productRightPos;
-    [SerializeField] private Vector3[] productInFirstPos;
-    [SerializeField] private float dropDist;
+    //Drop
+    [SerializeField] private Vector3 productInFirstPos;
+
+    //Snap
+    public GameObject[] snapPoints;
+    private float snapSensitivity = 2.0f;
+    private bool snap = false;
+    [SerializeField] private GameObject productHolder;
+    [SerializeField] private string snapTag = "ProductHolder";
+    
+    
 
     private void Start()
     {
-        for (int i = 0; i < product.Length; i++)
-        {
-            productInFirstPos[i] = product[i].transform.position;
-        }
-        
+        productInFirstPos = transform.position;
     }
 
     public void Update()
     {
-        Vector3 v3;
-
         if (Input.touchCount != 1)
-        {
-            dragging = false;
-
-            for (int i = 0; i < product.Length; i++)
-            {
-                product[i].transform.position = productInFirstPos[i];
-            }
-            
+        { 
+            dragging = false; 
+            transform.position = productInFirstPos;
             return;
         }
 
+        if (Input.touches[0].phase == TouchPhase.Began)
+        {
+            CheckHitProduct();
+        }
+
+        if (dragging && Input.touches[0].phase == TouchPhase.Moved)
+        {
+            DragProduct();
+        }
+
+        if (dragging && (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled))
+        {
+            DropProduct();
+        }
+            
+    }
+
+    void CheckHitProduct()
+    {
         Touch touch = Input.touches[0];
         Vector3 pos = touch.position;
+        
+        Ray ray = Camera.main.ScreenPointToRay(pos);
+        RaycastHit hit;
 
-        if (touch.phase == TouchPhase.Began)
+        if (Physics.Raycast(ray, out hit))
         {
-            Ray ray = Camera.main.ScreenPointToRay(pos);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit))
+            if (hit.collider.tag == "Product" )
             {
-                if (hit.collider.tag == "Product" )
-                {
-                    toDrag = hit.transform;
-                    dist = hit.transform.position.z - Camera.main.transform.position.z;
-                    v3 = new Vector3(pos.x, pos.y, dist);
-                    v3 = Camera.main.ScreenToWorldPoint(v3);
-                    offset = toDrag.position - v3;
-                    dragging = true;
-                }
+                toDrag = hit.transform;
+                dist = hit.transform.position.z - Camera.main.transform.position.z;
+                v3 = new Vector3(pos.x, pos.y, dist);
+                v3 = Camera.main.ScreenToWorldPoint(v3);
+                offset = toDrag.position - v3;
+                dragging = true;
             }
         }
 
-        if (dragging && touch.phase == TouchPhase.Moved)
-        {
-            v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
-            v3 = Camera.main.ScreenToWorldPoint(v3);
-            toDrag.position = v3 + offset;
-        }
-
-        if (dragging && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled))
-        {
-            for (int i = 0; i < Mathf.Min(product.Length, productRightPos.Length); ++i)
-            {
-                float Distance = Vector3.Distance(product[i].transform.position, productRightPos[i].transform.position);
-                
-                if (Distance < dist)
-                {
-                    isLocked = true;
-                    product[i].transform.position = productRightPos[i].transform.position;
-                }
-                else
-                {
-                    product[i].transform.position = productInFirstPos[i];
-                }
-                
-                dragging = false;
-            }
-            
-        }
     }
     
-
-    /*public void DropObjects()
+    void DragProduct()
     {
-        
-            float Distance = Vector3.Distance(product.transform.position, productRightPos.transform.position);
-
-            if (Distance < dist)
-            {
-                isLocked = true;
-                product.transform.position = productRightPos.transform.position;
-            }
-            else
-            {
-                product.transform.position = productInFirstPos;
-            } 
-        
-        
+        v3 = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, dist);
+        v3 = Camera.main.ScreenToWorldPoint(v3);
+        toDrag.position = v3 + offset;
+    }
+    
+    void DropProduct()
+    {
+        transform.position = productInFirstPos;
+        dragging = false;
     }*/
+    
+
 }
