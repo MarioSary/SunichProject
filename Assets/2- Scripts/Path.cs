b60a0c1b7bcc0a2c3ea9dd6314554fb9383b7080
Maryam Ansari <145547172+MarioSary@users.noreply.github.com>
2024-04-25 15:08:40 -0800

    Path updated

--- Assets/2- Scripts/Path.cs
+++ Assets/2- Scripts/Path.cs
@@ -6,6 +6,7 @@ public class Path : MonoBehaviour
 {
     [SerializeField] private Transform[] points;
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotationSpeed = 0.1f;
     private int pointsIndex;
 
     void Start()
@@ -18,8 +19,8 @@ public class Path : MonoBehaviour
     {
         if (pointsIndex <= points.Length -1)
         {
-            transform.position = Vector2.MoveTowards(transform.position, points[pointsIndex].transform.position,
-                moveSpeed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, points[pointsIndex].transform.position, moveSpeed * Time.deltaTime);
+            //transform.position = Vector3.Lerp(transform.position, points[pointsIndex].transform.position, moveSpeed * Time.deltaTime);
             transform.up = points[pointsIndex].transform.position - transform.position;
 
             if (transform.position == points[pointsIndex].transform.position)
